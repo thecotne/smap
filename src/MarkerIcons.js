098ba0bc464a213226eb7d57d1d918db3c650013
cotne nazarashvili <cotne.nazarashvili@gmail.com>
2015-05-23 14:15:11 +0400

    add docblocks

--- src/MarkerIcons.js
+++ src/MarkerIcons.js
@@ -1,5 +1,9 @@
 import {concat, find, propEq} from 'ramda';
 
+/**
+ * default marker icons
+ * @type {Array}
+ */
 var defaultIcons = [{
 	id: 0,
 	thumb: 'https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi.png',
@@ -9,12 +13,26 @@ var defaultIcons = [{
 }];
 
 export class MarkerIcons {
+	/**
+	 * MarkerIcons Collection
+	 * @param  {Array} marker icon object array
+	 */
 	constructor (icons = []) {
 		this.icons = concat(defaultIcons, icons);
 	}
+	/**
+	 * get by index
+	 * @param  {Number} index
+	 * @return {Object} marker icon object
+	 */
 	get (index) {
 		return this.icons[index];
 	}
+	/**
+	 * get by id
+	 * @param  {Number} id
+	 * @return {Object} marker icon object
+	 */
 	getById (id) {
 		return find(propEq('id', id), this.icons);
 	}
